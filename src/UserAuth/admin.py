b642fc3022562f60cd9d7ecc5062ea777ff3c889
PVL06 <pverl06@gmail.com>
2025-01-07 19:14:29 +0100

    remove authentication and project app, add UserAuth app with user custom model and migration

---
+++ src/UserAuth/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+
+from UserAuth.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'username',
+        'age',
+        'can_be_contacted',
+        'can_data_be_shared',
+        'last_login',
+        'date_joined',
+        'is_superuser',
+    )
+    search_fields = ('username',)
+    list_per_page = 50
