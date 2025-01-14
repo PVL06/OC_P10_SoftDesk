8e90cb54e9b13a38b9915eb1188d01f81f044e7e
PVL06 <pverl06@gmail.com>
2025-01-14 19:35:01 +0100

    add custom permission to project for contributor, refactor actions in project

--- src/SoftDeskAPI/permissions.py
+++ src/SoftDeskAPI/permissions.py
@@ -1,9 +1,10 @@
 from rest_framework import permissions
+from django.shortcuts import get_object_or_404
 
-ERROR_MESSAGE = 'You are not authorized for this action'
+from SoftDeskAPI.models import Contributors
+
 
 class UserPermissions(permissions.BasePermission):
-    message = ERROR_MESSAGE
 
     def has_permission(self, request, view):
         if view.action == 'create' and request.user.is_anonymous:
@@ -11,7 +12,7 @@ class UserPermissions(permissions.BasePermission):
         elif view.action != 'create' and request.user.is_authenticated:
             return True
         return False
-        
+
     def has_object_permission(self, request, view, obj):
         actions = ('partial_update', 'destroy')
         if view.action in actions and request.user == obj:
@@ -22,22 +23,16 @@ class UserPermissions(permissions.BasePermission):
 
 
 class ProjectPermissions(permissions.BasePermission):
-    message = ERROR_MESSAGE
 
-    def has_permission(self, request, view):
-        if request.user.is_authenticated:
-            return True
-        return False
-    
     def has_object_permission(self, request, view, obj):
-        actions = ('partial_update', 'destroy')
-        permissions = [
-            view.action in actions and request.user == obj.author,
-            view.action == 'retrieve' # todo verifier si l'user est dans les contributeurs
-        ]
-        if any(permissions):
+        is_contributor = Contributors.objects.filter(
+            project_id=obj,
+            contributor_id=request.user
+        ).exists()
+        author_actions = ('partial_update', 'destroy')
+
+        if view.action == 'retrieve' and is_contributor:
+            return True
+        elif view.action in author_actions and request.user == obj.author:
             return True
         return False
-
-class ContributorPermission(permissions.BasePermission):
-    message = ERROR_MESSAGE
--- src/SoftDeskAPI/views.py
+++ src/SoftDeskAPI/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
-from rest_framework import viewsets, response
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 
@@ -19,8 +20,46 @@ class UserViewset(viewsets.ModelViewSet):
         return user_model.objects.all()
 
 
-class ProjectViewset(viewsets.ModelViewSet):
+class ProjectActions:
+    @action(detail=True, methods=['post'])
+    def add_contributor(self, request, pk=None):
+        project = get_object_or_404(Project, pk=pk)
+        if request.user != project.author:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        
+        new_contributor = get_object_or_404(get_user_model(), username=request.POST.get('username'))
+        if self._check_user_in_project(new_contributor, pk):
+            return Response(
+                {'detail': 'This user is already a contributor'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        Contributors.objects.create(project_id=project, contributor_id=new_contributor)
+        return Response(
+            {'detail': f'The user {new_contributor} has been added to the contributors'}
+        )
 
+    @action(detail=True, methods=['post'])
+    def remove_contributor(self, request, pk=None):
+        if request.user != get_object_or_404(Project, pk=pk).author:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+        
+        contributor = get_object_or_404(get_user_model(), username=request.POST.get('username'))
+        if not self._check_user_in_project(contributor, pk):
+            return Response(
+                {'detail': f'{contributor} is not a contributor for this project'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        Contributors.objects.filter(project_id=pk, contributor_id=contributor).delete()
+        return Response(
+            {'detail': f'The user {contributor} has been deleted to the contributors'},
+        )
+
+    @staticmethod
+    def _check_user_in_project(user, pk):
+        return Contributors.objects.filter(project_id=pk, contributor_id=user).exists()
+
+
+class ProjectViewset(viewsets.ModelViewSet, ProjectActions):
     serializer_class = ProjectSerializer
     permission_classes = [ProjectPermissions]
 
@@ -30,25 +69,6 @@ class ProjectViewset(viewsets.ModelViewSet):
     
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
-
         project = get_object_or_404(Project, pk=serializer.data.get('id'))
         user = get_object_or_404(get_user_model(), id=self.request.user.pk)
-        Contributors.objects.create(
-            project_id=project,
-            contributor_id=user
-        )
-
-
-    @action(detail=True, methods=['post'])
-    def add_contributor(self, request, pk):
-        new_contributor = get_object_or_404(
-            get_user_model(),
-            username=request.POST.get('username')
-        )
-        project_contributors = Contributors.objects.filter(project_id=pk)
-        if new_contributor not in project_contributors:
-            return response.Response({'test': project_contributors[0].contributor_id.pk,
-                                      'new contrib pk': new_contributor.pk,
-                                      'project pk': pk,
-                                      })
-        return response.Response({'status': 'failed'})
+        Contributors.objects.create(project_id=project, contributor_id=user)
